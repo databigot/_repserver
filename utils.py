83247a24bb6108961a43934a94c3a8aa64862d3a
Jeremy Elliott <jeremy.elliott@tippr.com>
2012-04-03 20:44:55 +0000

    Did a number of changes.  Mostly to support the non-WEB generation of reports.
    added routines to send email:
    	+ email_senddoc()
    	+ email_wfiles()
    	+ email_simple()
    added routines to output data as text-table (eg.psql output) & html-table:
    	+ data_to_htmltable()
    	+ data_to_texttable(): 	| column A |   column B |
    				-------------------------
    				| JeremyE  |  3/14/2012 |
    changed the way the converter routines work: broke each into a pair of routines:
    	data_to_*() : convert data to specific format
    	*_response(): produce an http response appropriate for this type
    the main purpose for this change was to allow the same routines to be used for
    generating formatted documents for email sending.  Email needs docs, NOT http responses,
    so the logic needed to be pulled apart.
    Also the data_to_*() methods were extended to return the document, AND the content-type,
    and mime-type to use for this format.
    	+ data_to_gjson
    	+ gjson_response
    	+ data_to_json
    	+ json_response
    	+ data_to_csv
    	+ csv_response
    Added routines to store output files:
    	* store doc to s3
    	* store doc to local
    	* tinyurl  -- doesn't work yet!
    Added routines to allow caching datasets in mongo:
    	* list mongo fixtures
    	* load mongo fixture
    	* store as mongo

--- utils.py
+++ utils.py
@@ -4,8 +4,15 @@ import csv
 from cStringIO import StringIO
 import json
 import datetime
+import tempfile
+from jinja2 import Template
+
+import boto
+from settings import *
+import pymongo 
 
 class exporter(): 
+#TODO: this was a start idea, to group all the converters together...but never really built it.
 		"""
 			class to encapsulate: output this data D as output type X,
 			potential: text, html-table, json, csv.
@@ -15,7 +22,109 @@ class exporter():
 		""" 
 		pass
 
-def csv_out(ROWS, COLS, CONTEXT):
+#TODO: in all converters, handle the data-type, both pulling the datum, and formatting it.
+# especially note: 'u'-type, (and maybe 't'-type), handle 'percent', 'linkto', 'currency', etc.
+#		right now will die ugly in linkto, and percent.
+
+CSV_MIME='text/csv'; CSV_CTYPE='text/csv'
+def data_to_csv(ROWS, COLS, CONTEXT):
+    csvfile = StringIO();
+    csvout = csv.writer(csvfile)
+
+    csvout.writerow( [col['l'] for col in COLS] );
+
+    for row in ROWS:
+	csvout.writerow([ row[col['k']] 
+		for col in COLS] )
+
+    return  csvfile.getvalue(), CSV_MIME, CSV_CTYPE
+
+def csv_response(data, CONTEXT):
+    filename = '%s--%s.csv'%(CONTEXT.get('REPORTSLUG',''),
+		datetime.date.strftime(datetime.datetime.now(), '%Y%b%d_%I%M'))
+
+    response = Response(status=200, mimetype=CSV_MIME , content_type=CSV_CTYPE)
+    response.data = data
+    if True: #download as attachment/show in browser
+        response.headers['Content-Disposition'] = 'attachment; filename='+filename;	
+    return response
+
+
+
+HTML_MIME='text/html'; HTML_CTYPE='text/html'
+def data_to_htmltable(ROWS, COLS, CONTEXT):
+	"""
+		produce a simple html table of our data
+	"""
+	html_template = """
+	<table>
+		<thead>
+			<tr> 
+				{% for c in COLS %} 
+				<th> {{ c['l'] }} </th>
+				{% endfor %}
+			</tr>
+		</thead>
+		<tbody>
+			{% for r in ROWS %}
+			<tr>
+				{% for c in COLS %}
+				<td class="{{ c['k'] }}">{{ r[c['k']] }}</td>
+				{% endfor %}
+			</tr>
+			{% endfor %}
+		</tbody>
+	</table>
+	"""
+	return Template(html_template).render(
+		ROWS=ROWS, COLS=COLS, **CONTEXT), HTML_MIME, HTML_CTYPE
+
+def data_to_texttable(ROWS, COLS, CONTEXT):
+	"""
+	"""
+	textout = StringIO();
+	doc = ""	
+
+	col_width = {};tbl_width= 0	
+	for c in COLS:
+		col_name = c['k']
+		_wid = len(c['l'])
+		col_width[col_name] = _wid
+	formatter = {
+	#TODO: handle 'u'-type, handle 'percent', 'linkto', 'currency', etc.
+		None:		(lambda _val, _wid: _val.ljust(_wid))
+		,'string': 	(lambda _val, _wid: _val.ljust(_wid))
+		,'number':	(lambda _val, _wid: str(_val).rjust(_wid))
+		,'date':	(lambda _val, _wid: str(_val).rjust(_wid))
+		}
+	for r in ROWS:
+		for c in COLS:
+			col_name = c['k']
+			if not c['t'] :
+				col_width[col_name]= max(col_width[col_name], len(r[col_name]))
+
+	print >>textout, CONTEXT["title"].center(sum(col_width.itervalues(), len(COLS)-1))
+
+	# "xx".ljust(10), rjust(), center()
+	print >>textout, '+'.join(['-'*col_width[c['k']] for c in COLS])
+	print >>textout, '|'.join([c['l'].center(col_width[c['k']]) for c in COLS])
+	print >>textout, '+'.join(['-'*col_width[c['k']] for c in COLS])
+	for r in ROWS:			
+		print >>textout, '|'.join(
+			[formatter[c['t']](r[c['k']],col_width[c['k']]) 
+				for c in COLS])	
+	print >>textout, '+'.join(['-'*col_width[c['k']] for c in COLS])
+	print >>textout, '(%s rows)'% len(ROWS)
+	return textout.getvalue(), 'text/plain', 'text/plain'
+
+def unique_filename(format="txt", report="", by=""):
+	ext = {'html-table':'html', 'text-table':'txt','csv':'csv', 'json':'json', 'txt':'txt'}[format]
+	filename = '%s-%s-%s.%s'%(report
+			, by, datetime.date.strftime(datetime.datetime.now()
+			, '%Y%b%d_%I%M'), ext)
+	return filename
+
+def csv_out_simple(ROWS, COLS, CONTEXT):
     filename = '%s--%s.csv'%(CONTEXT.get('REPORTSLUG',''),
 		datetime.date.strftime(datetime.datetime.now(), '%Y%b%d_%I%M'))
     csvfile = StringIO();
@@ -31,47 +140,30 @@ def csv_out(ROWS, COLS, CONTEXT):
     response = Response(status=200, mimetype='text/csv', content_type='text/csv')
     response.data = data
     if True: #download as attachment/show in browser
-        response.headers['Content-Disposition'] = 'attachment; filename='+filename;	
-    return response
-
-def csv_out_simple(ROWS, COLS, CONTEXT):
-    filename = '%s--%s.csv'%(CONTEXT.get('REPORTSLUG',''),
-		datetime.date.strftime(datetime.datetime.now(), '%Y%b%d_%I%M'))
-    csvfile = StringIO();
-    csvout = csv.writer(csvfile)
-
-    csvout.writerow( COLS );
-
-    for row in ROWS:
-	csvout.writerow([ row[col] 
-		for col in COLS] )
-
-    data =  csvfile.getvalue()
-    response = Response(status=200, mimetype='text/csv', content_type='text/csv')
-    response.data = data
-    if True: #download as attachment/show in browser
     	response.headers['Content-Disposition'] = 'attachment; filename='+filename;	
     return response
 
-
-
-def json_out(ROWS, COLS, CONTEXT):
+JSON_MIME='application/json'; JSON_CTYPE='application/json'
+def data_to_json(ROWS, COLS, CONTEXT):
 #TODO: extra things to send: # of rows, query params, datetime, reportname, report version, 		
 	data = {
 		'COLS':	COLS,
 		'ROWS': ROWS
 		}
-		
+	return json.dumps(data), JSON_MIME, JSON_CTYPE
+
+def json_response(data, CONTEXT):	
 	#response = Response(status=200, mimetype='text/x-json', content_type='text/x-json')
-	response = Response(status=200, mimetype='application/json', content_type='application/json')
+	response = Response(status=200, mimetype=JSON_MIME, content_type=JSON_CTYPE)
 # other examples use application/javascript as the mimetype
 # restful spec and Wikipedia say: application/json
-	response.data = json.dumps(data)
+	response.data = data
 
 	return response
+#, JSON_MIME, JSON_CTYPE
 
-
-def gjson_out(ROWS, COLS, CONTEXT):
+GJSON_MIME='application/json'; GJSON_CTYPE='application/json'
+def data_to_gjson(ROWS, COLS, CONTEXT):
 #	cols = [{id: 'k', label:'l', type:'' 
 	out = "{"
 	out += "\n\tcols: [\n"
@@ -98,11 +190,14 @@ def gjson_out(ROWS, COLS, CONTEXT):
 		out += "]}"
 	out += "\n\t    ]"
 	out += "}"
-	response = Response(status=200, mimetype='application/json', content_type='application/json')
-	response.data = out
+	return out, GJSON_MIME, GJSON_CTYPE
+
+#	return status, mimetype, content_type, data
+
+def gjson_response(data, CONTEXT):
+	response = Response(status=200, mimetype=GJSON_MIME, content_type=GJSON_CTYPE)
+	response.data = data
 	return response
-
-
 
 def cache_report_request(context):
 	pass
@@ -113,8 +208,164 @@ def cache_save_dataset(context):
 def cache_fetch_dataset(context):
 	pass
 
+def store_doc_to_s3(doc,unique_name,headers=None, use_prefix=None):
+	""" 
+	take in data, store it in our s3 bucket, 
+	and return url
+	"""
+	s3 = boto.connect_s3(S3_ACCESS_KEY,S3_SECRET_KEY)
+	bucket = s3.get_bucket(S3_REPORT_BUCKET) #bucket to use for storing reports
+	
+	key = boto.s3.key.Key(bucket)
+
+	if use_prefix: unique_name = use_prefix+'/'+unique_name  #HACK until i figure out how to properly use prefixes.
+
+	key.key = unique_name 
+	key.set_contents_from_string(doc, headers=headers)
+
+	url = key.generate_url(100000000, force_http=True)
+	return url
+
+def store_as_mongo_fixture(unique_name, query, qualifiers, doc):
+	MONGO_HOST = 'localhost' #TODO: move to settings?
+	mongo_conn = pymongo.Connection(MONGO_HOST)
+	fixtures = mongo_conn.test.fixtures
+#	fixtures.save(name, base_query, qualifiers, data)
+		#datetime pulled, by_whom, queryversion, ...
+	if fixtures.find_one({'name':unique_name}):
+		raise Exception('one already exists')
+	#TODO: should I just overwrite always?
+	fixtures.insert({'name':unique_name, 'base_query':query, 'qualifiers': qualifiers, 'doc':doc})
+
+def load_mongo_fixture(unique_name):
+	MONGO_HOST = 'localhost' #TODO: move to settings?
+	mongo_conn = pymongo.Connection(MONGO_HOST)
+	fixtures = mongo_conn.test.fixtures
+	my_fixture = fixtures.find_one({'name':unique_name})
+
+	return my_fixture["doc"]
+
+def list_mongo_fixtures(query):
+	MONGO_HOST = 'localhost' #TODO: move to settings?
+	mongo_conn = pymongo.Connection(MONGO_HOST)
+	fixtures = mongo_conn.test.fixtures
+	l_fixtures= fixtures.find({'base_query':query})
+	if not l_fixtures: return None
+	return [x["name"] for x in fixtures.find({'base_query':query})]
 
 
+
+
+
+
+def url_to_tiny(url):
+	import urllib
+	tiny_maker = "http://tinyurl.com/api-create.php?"+ urllib.urlencode({"url":url})
+	try: 
+		return  urllib.urlopen(tiny_maker).read()
+	finally:
+		return None
+
+def store_doc_to_local(doc, unique_name):
+	"""
+	take in a doc, store it locally in a file,
+	and return filepath
+	"""
+	#TODO: add LOCAL_REPORT_BUCKET to settings.py
+	#filepath = LOCAL_REPORT_BUCKET or tempfile.gettempdir()
+	filepath =  tempfile.gettempdir()
+	filepath += '/'+unique_name
+	f = open(filepath, 'w+')
+	f.write(doc)
+	f.close()
+	return filepath
+
+def email_simple(addr_from="reporting@tippr.com",addr_to=[], subject="", body=None):
+	"""
+	Send simple text emails, (w/o attachments)
+	"""
+	import smtplib
+	from email.mime.text import MIMEText
+
+	msg = MIMEText(body)
+	msg['Subject'] = subject
+	msg['To'] = ','.join(addr_to)
+	msg['From'] = addr_from	
+	smtp = smtplib.SMTP('localhost')
+	smtp.sendmail(addr_from, addr_to, msg.as_string())
+	smtp.quit()
+
+def email_wfiles(addr_from="reporting@tippr.com",addr_to=[], subject="", body=None, files=[]):
+	"""
+	"""
+	import smtplib
+	from email.mime.text import MIMEText
+	from email.mime.multipart import MIMEMultipart
+	import mimetypes
+
+	msg = MIMEMultipart()
+	msg['Subject'] = subject
+	msg['To'] = ','.join(addr_to)
+	msg['From'] = addr_from	
+	
+	msg.attach(MIMEText(body))
+
+	for f in files:
+		if not os.path.isfile(f): continue
+		ctype, encoding = mimetypes.guess_type(f)
+		if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+           	 	ctype = 'application/octet-stream'
+        	maintype, subtype = ctype.split('/', 1)
+        	if maintype == 'text':
+            		fp = open(f)
+            		# Note: we should handle calculating the charset
+            		submsg = MIMEText(fp.read(), _subtype=subtype)
+            		fp.close()
+		else:
+			#TODO: FAIL!!
+			raise Exception("unrecognized, or non-text type: %s/%s"%(maintype, subtype))
+	    		continue
+		submsg.add_header('Content-Disposition', 'attachment', filename=f)
+		msg.attach(submsg)	
+		
+	smtp = smtplib.SMTP('localhost')
+	smtp.sendmail(addr_from, addr_to, msg.as_string())
+	smtp.quit()
+
+def email_senddoc(addr_from="reporting@tippr.com",addr_to=[], subject="", body=None, ctype='text/plain', doc=None, fname='report.txt'):
+	"""
+	"""
+	import smtplib
+	from email.mime.text import MIMEText
+	from email.mime.multipart import MIMEMultipart
+#	import mimetypes
+
+	msg = MIMEMultipart()
+	msg['Subject'] = subject
+	msg['To'] = ','.join(addr_to)
+	msg['From'] = addr_from	
+	
+	msg.attach(MIMEText(body))
+
+	if ctype is None: 
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+  	 	ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+	if maintype == 'text':
+		# Note: we should handle calculating the charset
+		submsg = MIMEText(doc, _subtype=subtype)
+	else:
+		#TODO: FAIL!!
+		raise Exception("unrecognized, or non-text type: %s/%s"%(maintype, subtype))
+	submsg.add_header('Content-Disposition', 'attachment', filename=fname)
+	msg.attach(submsg)	
+		
+	smtp = smtplib.SMTP('localhost')
+	smtp.sendmail(addr_from, addr_to, msg.as_string())
+	smtp.quit()
 
 
 
